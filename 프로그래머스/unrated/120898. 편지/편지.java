e21d4ae8301e8126ae7a10b888b32025617e196e
leesiyoung <148300486+leesy94@users.noreply.github.com>
2023-11-22 16:32:23 +0900

    [level unrated] Title: 편지, Time: 0.02 ms, Memory: 74.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120898. 편지/편지.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(String message) {
+        int answer = 0;
+        answer = message.length() * 2;
+        
+        return answer;
+    }
+}
