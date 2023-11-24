1b1e0f61b20c4ad0cfc50ad8b74dd71ca065a379
leesiyoung <148300486+leesy94@users.noreply.github.com>
2023-11-24 10:14:39 +0900

    [level unrated] Title: 암호 해독, Time: 1.52 ms, Memory: 72.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120892. 암호 해독/암호 해독.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(String cipher, int code) {
+        String answer = "";
+        for(int i = 1;i <= cipher.length()/code;i++){
+            answer+= String.valueOf(cipher.charAt(i*code-1));
+        }
+        return answer;
+    }
+}
