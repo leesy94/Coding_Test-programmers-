edbc585df9f8f62d1a97d8104c0ac32bb94c8be6
leesiyoung <148300486+leesy94@users.noreply.github.com>
2024-01-01 20:05:00 +0900

    [level unrated] Title: 소인수분해, Time: 0.02 ms, Memory: 72.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120852. 소인수분해/소인수분해.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n) {
+        
+        List<Integer> arr = new ArrayList<>();
+        for(int i = 2 ; i <=n ; i ++) {
+            if(n % i == 0) {                
+                while(n%i == 0) {
+                    n/=i;
+                }
+                arr.add(i);
+            }
+        }
+        int[] answer = new int[arr.size()];
+        for(int i = 0 ; i <arr.size() ; i ++) {
+            answer[i] = arr.get(i);
+        }
+        return answer;
+    }
+}
