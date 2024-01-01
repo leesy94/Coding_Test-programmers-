ac12b85f0383471ad0c74fbdfc4bf21b24cbe990
leesiyoung <148300486+leesy94@users.noreply.github.com>
2024-01-01 17:32:03 +0900

    [level unrated] Title: 2의 영역, Time: 1.18 ms, Memory: 104 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181894. 2의 영역/2의 영역.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] arr) {
+        int num1 = -1;
+        int num2 = -1;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 2) {
+                if (num1 == -1) {
+                    num1 = i;
+                } else {
+                    num2 = i;
+                }
+            }
+        }
+        
+        if (num1 != -1 && num2 != -1) {
+            return Arrays.copyOfRange(arr, num1, num2+1);
+        } else if (num1 == -1 && num2 == -1) {
+            return new int[]{-1};
+        } else {
+            return new int[]{2};
+        }
+    }
+}
